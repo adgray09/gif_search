5d70f565f5fcfb85658389be1b67c93378a129c9
ALEX_GRAY <alexandergray37@yahoo.com>
2019-09-16 19:23:01 -0700

    fixed api key and search bar/button, also deleted jinja notation

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+#I would like to give credit to Ryan Smith for helping me with this project. 
+#He helped me get the top 10 results for a websearch and much more. 
+
+
 from flask import Flask, render_template, request
 import requests
 import json
@@ -8,29 +12,43 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract the query term from url using request.args.get()
-    
-    params = {
-        'q': "search",
-        'key': "H5WM0TFR6PJ8",
-        "limit": 10,
-    }
-    r = requests.get("https://api.tenor.com/v1/search?", params = params)
+    name = request.args.get('search')
+
+  
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
+    params = {
+        'q': name,
+        'key': "H5WM0TFR6PJ8",
+        'limit': 10,
+    }
+
+    r = requests.get("https://api.tenor.com/v1/search?", params = params)
+
+    if r.status_code == 200:
+        # json.loads returns a JSON OBJECT
+        response_dict = json.loads(r.content)
+        # Retrieve results from response dict
+        top_ten = response_dict['results']
+        print(top_ten)
+        print("HERE__________________________")
+        print(len(top_ten['results']))
+        # if 
+
 
     # TODO: Make an API call to Tenor using the 'requests' library. For 
     # reference on how to use Tenor, see: 
     # https://tenor.com/gifapi/documentation
-   # def get_gif():
-   #     r = requests.get(
-   # "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
-    if r.status_code == 200:
-        top_10gifs = json.loads(r.content)
-        print(top_10gifs)
-    else:
-        top_10gifs = None
+    # def get_gif():
+    #     r = requests.get(
+    # "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+    #if r.status_code == 200:
+        #top_10gifs = json.loads(r.content)
+        #print(top_10gifs)
+    #else:
+        #top_10gifs = None
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
@@ -43,7 +61,7 @@ def index():
 
     return render_template("index.html")
     
-    index()
+    # index()
 
 if __name__ == '__main__':
     app.run(debug=True)
